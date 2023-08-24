a0f752a404f6d5203424785e1909f76ef6819bd5
gabriel <130491279+garojas22@users.noreply.github.com>
2023-08-24 07:15:46 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export function Home() {
+	const [selectedColor, setSelectedColor] = useState("");
+  
+	useEffect(() => {
+	  const interval = setInterval(() => {
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		if (selectedColor === "red") {
+		  setSelectedColor("yellow");
+		} else if (selectedColor === "yellow") {
+		  setSelectedColor("green");
+		} else if (selectedColor === "green") {
+		  setSelectedColor("red");
+		}
+	  }, 3000); 
+  
+	  return () => {
+		clearInterval(interval);
+	  };
+	}, [selectedColor]);
+
+return (
+
+	<div className="p-5">
+        <div className="trafic-light">
+            <div className="py-1">
+            	    <div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow-red" : "")}></div>
+            </div>
+            <div className="py-1">
+            	    <div onClick={() => setSelectedColor("yellow")} className= {"light yellow" + (selectedColor === "yellow" ? " glow-yellow" : "")}></div>
+            </div>
+            <div className="py-1">	
+            	    <div onClick={() => setSelectedColor("green")} className= {"light green" + (selectedColor === "green" ? " glow-green" : "")}></div>
+            </div>
+		</div>         
+	</div>
+
+);	
 };
 
 export default Home;
